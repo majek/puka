c503c3c9fb1fba2e36a6b48d1b2956accb9413c8
Jakub Šťastný aka Botanicus <knava.bestvinensis@gmail.com>
2010-11-19 19:45:14 +0000

    Expecting rabbitmq-codegen to be at the parent directory isn't a very good idea, let's use submodules for that.

--- codegen.py
+++ codegen.py
@@ -4,7 +4,7 @@ import os
 import sys
 import string
 
-sys.path.append(os.path.join("..","rabbitmq-codegen"))
+sys.path.append(os.path.join("vendor", "rabbitmq-codegen"))
 from amqp_codegen import *
 
 import codegen_helpers
