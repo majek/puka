ee7ef70b0be84d4a39c0a13da08888cae1b7394d
Marek Majkowski <majek04@gmail.com>
2011-09-20 14:33:58 +0100

    Support RabbitMQ url scheme, as defined by David Wragg.
    We don't adhere to one aspect - vhost part is not allowed to be empty in Puka implementation.

--- puka/connection.py
+++ puka/connection.py
@@ -310,17 +310,42 @@ def parse_amqp_url(amqp_url):
     ('guest', 'guest', '/', 'host', 5672)
     >>> parse_amqp_url('amqp://host')
     ('guest', 'guest', '/', 'host', 5672)
+    >>> parse_amqp_url('amqp://user:pass@host:10000/vhost')
+    ('user', 'pass', 'vhost', 'host', 10000)
+    >>> parse_amqp_url('amqp://user%61:%61pass@ho%61st:10000/v%2fhost')
+    ('usera', 'apass', 'v/host', 'hoast', 10000)
+    >>> parse_amqp_url('amqp://')
+    ('guest', 'guest', '/', 'localhost', 5672)
+    >>> parse_amqp_url('amqp://:@/') # this is a violation, vhost should be=''
+    ('', '', '/', 'localhost', 5672)
+    >>> parse_amqp_url('amqp://user@/')
+    ('user', 'guest', '/', 'localhost', 5672)
+    >>> parse_amqp_url('amqp://user:@/')
+    ('user', '', '/', 'localhost', 5672)
+    >>> parse_amqp_url('amqp://host')
+    ('guest', 'guest', '/', 'host', 5672)
+    >>> parse_amqp_url('amqp:///vhost')
+    ('guest', 'guest', 'vhost', 'localhost', 5672)
+    >>> parse_amqp_url('amqp://host/')
+    ('guest', 'guest', '/', 'host', 5672)
+    >>> parse_amqp_url('amqp://host/%2f%2f')
+    ('guest', 'guest', '//', 'host', 5672)
+    >>> parse_amqp_url('amqp://[::1]')
+    ('guest', 'guest', '/', '::1', 5672)
     '''
     assert amqp_url.startswith('amqp://'), "Only amqp:// protocol supported."
     # urlsplit doesn't know how to parse query when scheme is amqp,
     # we need to pretend we're http'
     o = urlparse.urlsplit('http://' + amqp_url[len('amqp://'):])
-    username = urllib.unquote(o.username) if o.username else 'guest'
-    password = urllib.unquote(o.password) if o.password else 'guest'
+    username = urllib.unquote(o.username) if o.username is not None else 'guest'
+    password = urllib.unquote(o.password) if o.password is not None else 'guest'
 
     path = o.path[1:] if o.path.startswith('/') else o.path
+    # We do not support empty vhost case. Empty vhost is treated as
+    # '/'. This is mostly for backwards compatibility, and the fact
+    # that empty vhost is not very useful.
     vhost = urllib.unquote(path) if path else '/'
-    host = o.hostname or 'localhost'
+    host = urllib.unquote(o.hostname) if o.hostname else 'localhost'
     port = o.port if o.port else 5672
     return (username, password, vhost, host, port)
 
