b567cf5b08a26d40c56f630de6ff9a646f92f5d7
Ralf Schmitt <ralf@systemexit.de>
2012-12-11 23:21:37 +0100

    convert test_limits.py to py.test

--- tests/test_limits.py
+++ tests/test_limits.py
@@ -1,27 +1,15 @@
-import os
-import unittest
-import puka
-import random
-import time
+#! /usr/bin/env py.test
 
-AMQP_URL=os.getenv('AMQP_URL', 'amqp:///')
 
-class TestLimits(unittest.TestCase):
-    def test_parallel_queue_declare(self):
-        qname = 'test%s' % (random.random(),)
-        msg = '%s' % (random.random(),)
+def test_parallel_queue_declare(client, makeid, msg):
+    qname = makeid()
 
-        client = puka.Client(AMQP_URL)
-        promise = client.connect()
+    queues = [qname + '.%s' % (i,) for i in xrange(100)]
+    promises = [client.queue_declare(queue=q) for q in queues]
+
+    for promise in promises:
         client.wait(promise)
 
-        queues = [qname+'.%s' % (i,) for i in xrange(100)]
-        promises = [client.queue_declare(queue=q) for q in queues]
-
-        for promise in promises:
-            client.wait(promise)
-
-        promises = [client.queue_delete(queue=q) for q in queues]
-        for promise in promises:
-            client.wait(promise)
-
+    promises = [client.queue_delete(queue=q) for q in queues]
+    for promise in promises:
+        client.wait(promise)
