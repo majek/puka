31ef0a2a3981a299b9d6d1e3f490f00ab49a78d3
Stan Lagun <slagun@mirantis.com>
2013-06-28 00:22:36 +0400

    Added SSL support (amqps:// scheme)

--- puka/connection.py
+++ puka/connection.py
@@ -2,6 +2,7 @@ import errno
 import logging
 import select
 import socket
+import ssl
 import struct
 import time
 import urllib
@@ -44,18 +45,18 @@ class Connection(object):
         self.channels = channel.ChannelCollection()
         self.promises = promise.PromiseCollection(self)
 
-        (self.username, self.password, self.vhost, self.host, self.port) = \
-            parse_amqp_url(str(amqp_url))
+        (self.username, self.password, self.vhost,
+         self.host, self.port, self.ssl) = parse_amqp_url(str(amqp_url))
 
         self.client_properties = client_properties
 
         self.heartbeat = heartbeat
+        self._needs_ssl_handshake = False
 
     def _init_buffers(self):
         self.recv_buf = simplebuffer.SimpleBuffer()
         self.recv_need = 8
         self.send_buf = simplebuffer.SimpleBuffer()
-
 
     def fileno(self):
         return self.sd.fileno()
@@ -88,7 +89,47 @@ class Connection(object):
         except socket.error, e:
             if e.errno not in (errno.EINPROGRESS, errno.EWOULDBLOCK):
                 raise
+
+        if self.ssl:
+            self.sd = self._wrap_socket(self.sd)
+            self._needs_ssl_handshake = True
+
         return machine.connection_handshake(self)
+
+    def _wrap_socket(self, sock):
+        """Wrap the socket for connecting over SSL.
+        :rtype: ssl.SSLSocket
+        """
+        return ssl.wrap_socket(sock, do_handshake_on_connect=False)
+
+    def _do_ssl_handshake(self, timeout=None):
+        """Perform SSL handshaking
+        """
+        if not self._needs_ssl_handshake:
+            return False
+        if timeout is not None:
+            t1 = time.time() + timeout
+        else:
+            td = None
+            t1 = None
+
+        while True:
+            if timeout is not None:
+                t0 = time.time()
+                td = t1 - t0
+                if td < 0:
+                    break
+            try:
+                self.sd.do_handshake()
+                self._needs_ssl_handshake = False
+                break
+            except ssl.SSLError, e:
+                if e.args[0] == ssl.SSL_ERROR_WANT_READ:
+                    select.select([self.sd], [], [])
+                elif e.args[0] == ssl.SSL_ERROR_WANT_WRITE:
+                    select.select([], [self.sd], [])
+                else:
+                    raise
 
     def on_read(self):
         try:
@@ -220,6 +261,8 @@ class Connection(object):
             promise_numbers = [promise_numbers]
         promise_numbers = set(promise_numbers)
 
+        self._do_ssl_handshake(timeout=timeout)
+
         # Make sure the buffer is flushed if possible before entering
         # the loop. We may return soon, and the user has no way to
         # figure out if the write buffer was flushed or not - (ie: did
@@ -280,6 +323,8 @@ class Connection(object):
         else:
             td = None
         self._loop_break = False
+
+        self._do_ssl_handshake(timeout=timeout)
 
         while True:
             self.run_any_callbacks()
@@ -349,50 +394,54 @@ class Connection(object):
 def parse_amqp_url(amqp_url):
     '''
     >>> parse_amqp_url('amqp:///')
-    ('guest', 'guest', '/', 'localhost', 5672)
+    ('guest', 'guest', '/', 'localhost', 5672, False)
     >>> parse_amqp_url('amqp://a:b@c:1/d')
-    ('a', 'b', 'd', 'c', 1)
+    ('a', 'b', 'd', 'c', 1, False)
     >>> parse_amqp_url('amqp://g%20uest:g%20uest@host/vho%20st')
-    ('g uest', 'g uest', 'vho st', 'host', 5672)
+    ('g uest', 'g uest', 'vho st', 'host', 5672, False)
     >>> parse_amqp_url('http://asd')
     Traceback (most recent call last):
       ...
     AssertionError: Only amqp:// protocol supported.
     >>> parse_amqp_url('amqp://host/%2f')
-    ('guest', 'guest', '/', 'host', 5672)
+    ('guest', 'guest', '/', 'host', 5672, False)
     >>> parse_amqp_url('amqp://host/%2fabc')
-    ('guest', 'guest', '/abc', 'host', 5672)
+    ('guest', 'guest', '/abc', 'host', 5672, False)
     >>> parse_amqp_url('amqp://host/')
-    ('guest', 'guest', '/', 'host', 5672)
+    ('guest', 'guest', '/', 'host', 5672, False)
     >>> parse_amqp_url('amqp://host')
-    ('guest', 'guest', '/', 'host', 5672)
+    ('guest', 'guest', '/', 'host', 5672, False)
     >>> parse_amqp_url('amqp://user:pass@host:10000/vhost')
-    ('user', 'pass', 'vhost', 'host', 10000)
+    ('user', 'pass', 'vhost', 'host', 10000, False)
     >>> parse_amqp_url('amqp://user%61:%61pass@ho%61st:10000/v%2fhost')
-    ('usera', 'apass', 'v/host', 'hoast', 10000)
+    ('usera', 'apass', 'v/host', 'hoast', 10000, False)
     >>> parse_amqp_url('amqp://')
-    ('guest', 'guest', '/', 'localhost', 5672)
+    ('guest', 'guest', '/', 'localhost', 5672, False)
     >>> parse_amqp_url('amqp://:@/') # this is a violation, vhost should be=''
-    ('', '', '/', 'localhost', 5672)
+    ('', '', '/', 'localhost', 5672, False)
     >>> parse_amqp_url('amqp://user@/')
-    ('user', 'guest', '/', 'localhost', 5672)
+    ('user', 'guest', '/', 'localhost', 5672, False)
     >>> parse_amqp_url('amqp://user:@/')
-    ('user', '', '/', 'localhost', 5672)
+    ('user', '', '/', 'localhost', 5672, False)
     >>> parse_amqp_url('amqp://host')
-    ('guest', 'guest', '/', 'host', 5672)
+    ('guest', 'guest', '/', 'host', 5672, False)
     >>> parse_amqp_url('amqp:///vhost')
-    ('guest', 'guest', 'vhost', 'localhost', 5672)
+    ('guest', 'guest', 'vhost', 'localhost', 5672, False)
     >>> parse_amqp_url('amqp://host/')
-    ('guest', 'guest', '/', 'host', 5672)
+    ('guest', 'guest', '/', 'host', 5672, False)
     >>> parse_amqp_url('amqp://host/%2f%2f')
-    ('guest', 'guest', '//', 'host', 5672)
+    ('guest', 'guest', '//', 'host', 5672, False)
     >>> parse_amqp_url('amqp://[::1]')
-    ('guest', 'guest', '/', '::1', 5672)
+    ('guest', 'guest', '/', '::1', 5672, False)
+    >>> parse_amqp_url('amqps://user:pass@host:10000/vhost')
+    ('user', 'pass', 'vhost', 'host', 10000, True)
     '''
-    assert amqp_url.startswith('amqp://'), "Only amqp:// protocol supported."
+    assert amqp_url.startswith('amqp://') or \
+        amqp_url.startswith('amqps://'), \
+        "Only amqp:// and amqps:// protocols are supported."
     # urlsplit doesn't know how to parse query when scheme is amqp,
     # we need to pretend we're http'
-    o = urlparse.urlsplit('http://' + amqp_url[len('amqp://'):])
+    o = urlparse.urlsplit('http' + amqp_url[len('amqp'):])
     username = urllib.unquote(o.username) if o.username is not None else 'guest'
     password = urllib.unquote(o.password) if o.password is not None else 'guest'
 
@@ -403,7 +452,8 @@ def parse_amqp_url(amqp_url):
     vhost = urllib.unquote(path) if path else '/'
     host = urllib.unquote(o.hostname) if o.hostname else 'localhost'
     port = o.port if o.port else 5672
-    return (username, password, vhost, host, port)
+    ssl = o.scheme == 'https'
+    return (username, password, vhost, host, port, ssl)
 
 def set_ridiculously_high_buffers(sd):
     '''
@@ -434,3 +484,4 @@ def set_close_exec(fd):
         fcntl.fcntl(fd, fcntl.F_SETFD, flags | fcntl.FD_CLOEXEC)
     except ImportError:
         pass
+
