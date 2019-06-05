b9a292867d358da59f89f624857ee7917d330a2f
Justin Israel <justinisrael@gmail.com>
2019-06-06 09:24:44 +1200

    Fix broken test not unpacking correct number of return values
    Signed-off-by: Justin Israel <justinisrael@gmail.com>

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -24,7 +24,7 @@ class TestConnection(base.TestCase):
     # The following tests take 3 seconds each, due to Rabbit.
     def test_wrong_user(self):
         (username, password, vhost, host, port) = \
-            puka.connection.parse_amqp_url(self.amqp_url)
+            puka.connection.parse_amqp_url(self.amqp_url)[:5]
 
         client = puka.Client('amqp://%s:%s@%s:%s%s' % \
                                  (username, 'wrongpass', host, port, vhost))
